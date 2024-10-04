728a2d526352f8f96a28bd9adf4f1853ba53cc41
tung1101 <tungminh1101@gmail.com>
2024-10-05 00:09:26 +0700

    update

--- tests/lesson-3/06-forOf.js
+++ tests/lesson-3/06-forOf.js
@@ -1,14 +1,5 @@
 let arr = [1, 2, 3, 1, 2, 4, 5, 5]
-
 // Bài 1:
-let count;
-let x=arr[0];
-for (let item of arr) {
-if (item - x===0) {
-    x = item;
-}
-    console.log(item);
-}
 // Bài 2 :
 let str = 'Playwright';
 let newArr = [];
@@ -21,18 +12,18 @@ for (let char of newStr) {
 }
 console.log(newArr);
 // Bài 3:
-for (let i = 0; i < arr.length-1; i++) {
-    count =0;
-    for (let j = 1; j < arr.length; j++) {
-        if (arr[i] === arr[j]) {
-           count++;
-        }
-    }
-    if (count === 1) {
-        newArr.push(arr[i]);
-    }
-}
-console.log(newArr);
+// for (let i = 0; i < arr.length-1; i++) {
+//     count =0;
+//     for (let j = 1; j < arr.length; j++) {
+//         if (arr[i] === arr[j]) {
+//            count++;
+//         }
+//     }
+//     if (count === 1) {
+//         newArr.push(arr[i]);
+//     }
+// }
+// console.log(newArr);
 
 
 
