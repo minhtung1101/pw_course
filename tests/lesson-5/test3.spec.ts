f311b98cf52a88aa58dea2b4150f297a324c7085
tung1101 <tungminh1101@gmail.com>
2024-10-13 16:12:25 +0700

    a

--- tests/lesson-5/test3.spec.ts
+++ tests/lesson-5/test3.spec.ts
@@ -3,15 +3,17 @@ import { test } from "@playwright/test";
 test('bai 3', async ({ page }) => {
     await page.goto('https://material.playwrightvn.com');
     await page.getByRole('link', { name: 'Bài học 3: Todo page' }).click();
-    for (let i = 1; i < 10; i++) {
+    for (let i = 1; i < 101; i++) {
         await page.fill("//input[@type='text']", 'Todo ' + i);
         await page.click("//button[@id='add-task']");
+
     }
-    
-    for (let i = 1; i < 10; i++) {
+    await page.waitForTimeout(1000); 
+
+    for (let i = 1; i < 101; i++) {
         if (i % 2 === 1) {
-            await page.click(`//button[@id="todo-${i}-delete"]`, { timeout: 5000 });
-            await page.on('dialog', dialog =>  dialog.accept());
+            await page.click(`//button[@id="todo-${i}-delete"]`);
+            await page.on('dialog', dialog => dialog.accept());
         }
     }
 });
